b5204133f9ce2ed91a383c337678500ade356db9
aldemar niño cortazar <anino79@misena.edu.co>
2025-06-15 15:37:17 -0500

    changes in cors

--- config/server.ts
+++ config/server.ts
@@ -4,4 +4,17 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+
+  cors: {
+    headers: env.array('CORS_HEADERS', ['Content-Type', 'Authorization']),
+    methods: env.array('CORS_METHODS', ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']),
+    credentials: env.bool('CORS_CREDENTIALS', true),
+    enabled: true,
+    origin: [
+      'http://localhost:3000', //
+      'https://www.fsolidaritycolombia.org', 
+      'https://fsolidaritycolombia.org', 
+      'https://api.fsolidaritycolombia.org' 
+    ],
+  }
 });
