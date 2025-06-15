64bab47614c8b47f2e94b33aed41329bad397195
aldemar niño cortazar <anino79@misena.edu.co>
2025-06-15 15:44:51 -0500

    all domains

--- config/server.ts
+++ config/server.ts
@@ -10,11 +10,12 @@ export default ({ env }) => ({
     methods: env.array('CORS_METHODS', ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']),
     credentials: env.bool('CORS_CREDENTIALS', true),
     enabled: true,
-    origin: [
-      'http://localhost:3000', //
-      'https://www.fsolidaritycolombia.org', 
-      'https://fsolidaritycolombia.org', 
-      'https://api.fsolidaritycolombia.org' 
-    ],
+    origin: env('CORS_ORIGIN', '*'),
+    maxAge: env.int('CORS_MAX_AGE', 3600),
+    expose: env.array('CORS_EXPOSE', []), 
+    keepHeadersOnError: env.bool('CORS_KEEP_HEADERS_ON_ERROR', false),
+    preflightContinue: env.bool('CORS_PREFLIGHT_CONTINUE', false),
+    preflightMaxAge: env.int('CORS_PREFLIGHT_MAX_AGE', 86400),
+    
   }
 });
